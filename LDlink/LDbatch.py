513fea631676335382d99b386989b61482360fbf
Tony Hall <tony.hall@nih.gov>
2016-10-26 13:49:48 -0400

    correctly sending data to queue

--- LDlink/LDbatch.py
+++ LDlink/LDbatch.py
@@ -1,59 +1,168 @@
 #!/usr/bin/env python
-import json
-import logging
-from stompest.config import StompConfig
 from stompest.sync import Stomp
-import QueueProcessor
+from QueueProcessor import QueueProcessor
+import math
+import smtplib
 import os
 import sys
 import time
 from flask import jsonify, json
+from LDproxy import calculate_proxy
 
-def toQueue(email, tokenId):
+def toQueue(email=None, tokenId=None):
     resp = ""
     try:
-        qp = QueueProcessor(
-            PRODUCT_NAME = "LDlink Batch Processing Module",
-            CONFIG_FILE = r"QueueConfig.ini",
-            Q_NAME = 'queue.name',
-            Q_URL = 'queue.url',
-            Q_PORT = 'queue.port',
-            Q_ERROR = 'queue.error.name',
-            MAIL_HOST = 'mail.host',
-            MAIL_ADMIN = 'mail.admin')
-
-        print "after qp"
-
-        batchFile = open(os.path.join('tmp', "inputBatch_"+ tokenId))
+        batchFile = os.path.join('tmp', "inputBatch_"+ tokenId)
 
         ts = time.strftime("%Y-%m-%d")
         data = json.dumps({ 
-            "filepath": batchFile.name,
+            "filepath": batchFile,
             "token": tokenId,
             "timestamp": ts
         })
 
-        print "before client connect"
-        print "Stomp( {0}, {1} )".format(qp.Q_URL, qp.Q_PORT)
-        
-        client = Stomp("tcp://{0}:{1}".format(qp.Q_URL, qp.Q_PORT))
+        client = Stomp(qp.QUEUE)
 
-        #opening connection to queue
+        # opening connection to queue
         client.connect()
+        print "Connected to queue..."
 
-        print qp.Q_NAME
-        print data
-
+        # sending to queue
         client.send(qp.Q_NAME, data)
-        client.disconnect()
 
-        resp = jsonify({ "message": "The batch process has begun. The results will be emailed to " + email })
+        # disconnecting
+        client.disconnect()
+        print "disconnected from queue..."
+        return
     except Exception, e:
-        errorType, error = sys.exc_info()[:2]
+        print "In Exeception toQueue"
+        errorType, error,traceback = sys.exc_info()
         print errorType
         print error
-        print sys.exc_info()[2]
-        resp = jsonify({ "message" : "The batch process was not executed due to an error. \n" + e.args })
+        print traceback
+        print traceback.tb_lineno
+        print __FILE__
+        resp = jsonify({ "message" : "The batch process was not executed due to an error.\n" + e.args.join(', ') })
         resp.status_code = 400
-    finally:
         return resp
+
+def queueConsumer(self, client, frame):
+    resp = ""
+    files = []
+    try:
+        print "<----- Frame body"
+        print frame.body
+        print frame.body.decode()
+
+        parameters = json.loads(frame.body)
+
+        print "<----------- Params"
+        print parameters
+        token = parameters['token']
+        batchFilename = parameters['batchFile']
+        recipient = parameters['recipientEmail']
+        timestamp = parameters['timestamp']
+
+        print token
+        print batchFilename
+
+        # --------------- THIS MAY CHANGE
+        with open(batchFilename) as content_file:
+            contents = content_file.read()
+
+        fileContents = json.loads(contents)
+        print fileContents
+
+        print "After calculation"
+
+        link = "<a href='{0}'> Here </a>".format(urllib.unquote(data['queue']['url']))
+        header = "<h2>{0}</h2>".format(qp.PRODUCT_NAME)
+        body = """<div 
+        style='background-color:white;border-top:25px solid #142830;border-left:2px solid #142830;border-right:2px solid #142830;border-bottom:2px solid #142830;padding:20px'>
+        Hello,<br><p>Here are the results you requested on {0} from the {1}.</p>
+        <p><div style='margin:20px auto 40px auto;width:200px;text-align:center;font-size:14px;font-weight:bold;padding:10px;line-height:25px'>
+        <div style='font-size:24px;'><a href='{2}'>View Results</a></div></div></p>
+        <p>The results will be available online for the next 14 days.</p></div>""".format(timestamp, qp.PRODUCT_NAME, urllib.unquote(data['queue']['url']) )
+
+        footer = """<div><p>(Note:  Please do not reply to this email. If you need assistance, please contact NCILDlinkWebAdmin@mail.nih.gov)</p></div>
+        <div style="background-color:#ffffff;color:#888888;font-size:13px;line-height:17px;font-family:sans-serif;text-align:left">
+          <p>
+              <strong>About<em>""" + qp.PRODUCT_NAME + """</em></strong></em><br>
+              <!-- LDBatch E-Mail description -->
+              <strong>For more information, visit <a target="_blank" style="color:#888888" href="http://analysistools.nci.nih.gov">analysistools.nci.nih.gov/ldlink?tab=ldbatch</a>
+              </strong>
+          </p>
+          <p style="font-size:11px;color:#b0b0b0">If you did not request a calculation please ignore this email. Your privacy is important to us.  
+          Please review our <a target="_blank" style="color:#b0b0b0" href="http://www.cancer.gov/policies/privacy-security">Privacy and Security Policy</a>.
+          </p>
+          <p align="center"><a href="http://cancercontrol.cancer.gov/">Division of Cancer Control & Population Sciences</a>, 
+          <span style="white-space:nowrap">a Division of <a href="www.cancer.gov">National Cancer Institute</a></span><br>
+          BG 9609 MSC 9760 | 9609 Medical Center Drive | Bethesda, MD 20892-9760 | <span style="white-space:nowrap"><a target="_blank" value="+18004006916" href="tel:1-800-422-6237">1-800-4-CANCER</a></span>
+          </p></div>"""
+        if self.EMAIL_BODY is None:
+            message = """<head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'><title>html title</title></head><body>{0}{1}{2}</body>""".format(header, body, footer)
+        else:
+            message = """<head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'><title>html title</title></head><body>{0}</body>""".format(qp.EMAIL_BODY)
+
+        print "sending E-Mail"
+        createMail(sendTo=data['queue']['email'],message=message, files=files)
+        print "Queue job DONE!"
+
+    except Exception, e:
+        errorType, error,traceback = sys.exc_info()
+        print errorType
+        print error
+        print traceback
+        print traceback.tb_lineno
+        print __FILE__
+
+def processCleanup(self, connect):
+    print "in proccessCleanup, Cleaning up...."
+    print "<-- Add code for cleanup after queue process complete, if necessary -->"
+
+def createMail(self, sendTo=None, message=None, files=[]):
+    print "sending message"
+    recipients = []
+
+    try:
+        if isinstance(sendTo, None):
+            recipients = [sendTo]
+
+        packet = MIMEMultipart()
+        packet['Subject'] = self.PRODUCT_NAME + " Results"
+        packet['From'] = self.PRODUCT_NAME + " <do.not.reply@nih.gov>"
+        packet['To'] = ", ".join(recipients)
+
+        packet.attach(MIMEText(message, 'html'))
+
+        for file in files:
+            with open(file,"rb") as openfile:
+                mimeApp = MIMEApplication(
+                    openfile.read(),
+                    Content_Disposition = 'attachment; filename="%s"' % os.path.basename(file),
+                    Name = os.path.basename(file)
+                )       
+                packet.attach(mimeApp)
+
+        smtp = smtplib.SMTP(self.MAIL_HOST)
+        smtp.sendmail("do.not.reply@nih.gov", recipients, packet.as_string())
+    except Exception, e:
+        errorType, error,traceback = sys.exc_info()
+        print errorType
+        print error
+        print traceback
+        print traceback.tb_lineno
+        print __FILE__
+        resp = jsonify({ "message": e.args })
+        resp.status_code = 400
+
+qp = QueueProcessor(
+    PRODUCT_NAME = "LDlink Batch Processing Module",
+    CONFIG_FILE = r"config.ini",
+    Q_NAME = 'queue.name',
+    Q_URL = 'queue.url',
+    Q_ERROR = 'queue.error.name',
+    MAIL_HOST = 'mail.host',
+    MAIL_ADMIN = 'mail.admin',
+    PROCESS_FUNCT = calculate_proxy,
+    CONSUMER_FUNCT = queueConsumer)
--- LDlink/QueueProcessor.py
+++ LDlink/QueueProcessor.py
@@ -1,29 +1,25 @@
-import json
-import math
+
 import os
 import sys
-
-import rpy2.robjects as robjects
-import smtplib
 import time
 import logging
 import urllib
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from stompest.async import Stomp
 from stompest.async.listener import SubscriptionListener
 from stompest.async.listener import DisconnectListener
 from stompest.config import StompConfig
+from stompest.protocol import StompSpec
 from twisted.internet import reactor, defer
 from PropertyUtil import PropertyUtil
-from flask import jsonify
 
 class QueueProcessor(SubscriptionListener):
     CONFIG_FILE = None
     QUEUE_CONFIG = None
     Q_NAME = None
     Q_URL = None
-    Q_PORT = None
     Q_ERROR = None
     MAIL_HOST = None
     MAIL_ADMIN = None
@@ -31,159 +27,55 @@ class QueueProcessor(SubscriptionListener):
     PREFETCH = 100
     PRODUCT_NAME = None
     QUEUE = None
-
-    def createMail(self, sendTo=None, message=None, files=[]):
-        try:
-            print "sending message"
-            recipients = []
-            if sendTo is not None:
-                recipients = [ sendTo ]
-
-            packet = MIMEMultipart()
-            packet['Subject'] = self.PRODUCT_NAME + " Results"
-            packet['From'] = self.PRODUCT_NAME + " <do.not.reply@nih.gov>"
-            packet['To'] = ", ".join(recipients)
-
-            packet.attach(MIMEText(message, 'html'))
-
-            for file in files:
-                with open(file,"rb") as openfile:
-                    packet.attach(MIMEApplication(
-                        openfile.read(),
-                        Content_Disposition = 'attachment; filename="%s"' % os.path.basename(file),
-                        Name = os.path.basename(file)
-                    ))
-
-            print self.MAIL_HOST
-            smtp = smtplib.SMTP(self.MAIL_HOST)
-            smtp.sendmail("do.not.reply@nih.gov", recipients, packet.as_string())
-        except Exception, e:
-            print "Error Type: {0} Error: {1} Trace: {2}".format(errorType, error, traceback)
-            resp = jsonify({ "message": e.args })
-            resp.status_code = 400
-
-    def queueConsumer(self, client, frame):
-        resp = ""
-        try:
-            files = []
-            print "<----- Frame body"
-            print frame.body
-            print frame.body.decode()
-            parameters = json.loads(frame.body)
-
-            print "<----------- Params"
-            print parameters
-            token = parameters['token']
-            batchFilename = parameters['batchFile']
-            recipient = parameters['recipientEmail']
-            fname = os.path.join("tmp", batchFilename)
-            timestamp = ['timestamp']
-
-            print token
-            print fname
-
-            # --------------- THIS MAY CHANGE
-            with open(fname) as content_file:
-                contents = content_file.read()
-
-            fileContents = json.loads(contents)
-            print fileContents
-
-            print "After calculation"
-
-            link = "<a href='{0}'> Here </a>".format(urllib.unquote(data['queue']['url']))
-            header = "<h2>{0}</h2>".format(self.PRODUCT_NAME)
-            body = """<div 
-            style='background-color:white;border-top:25px solid #142830;border-left:2px solid #142830;border-right:2px solid #142830;border-bottom:2px solid #142830;padding:20px'>
-            Hello,<br><p>Here are the results you requested on {0} from the {1}.</p>
-            <p><div style='margin:20px auto 40px auto;width:200px;text-align:center;font-size:14px;font-weight:bold;padding:10px;line-height:25px'>
-            <div style='font-size:24px;'><a href='{2}'>View Results</a></div></div></p>
-            <p>The results will be available online for the next 14 days.</p></div>""".format(parameters['timestamp'], self.PRODUCT_NAME, urllib.unquote(data['queue']['url']))
-
-            footer = """<div><p>(Note:  Please do not reply to this email. If you need assistance, please contact NCILDlinkWebAdmin@mail.nih.gov)</p></div>
-            <div style="background-color:#ffffff;color:#888888;font-size:13px;line-height:17px;font-family:sans-serif;text-align:left">
-              <p>
-                  <strong>About<em>""" + self.PRODUCT_NAME + """</em></strong></em><br>
-                  <!-- LDBatch E-Mail description -->
-                  <strong>For more information, visit <a target="_blank" style="color:#888888" href="http://analysistools.nci.nih.gov">analysistools.nci.nih.gov/ldlink?tab=ldbatch</a>
-                  </strong>
-              </p>
-              <p style="font-size:11px;color:#b0b0b0">If you did not request a calculation please ignore this email. Your privacy is important to us.  
-              Please review our <a target="_blank" style="color:#b0b0b0" href="http://www.cancer.gov/policies/privacy-security">Privacy and Security Policy</a>.
-              </p>
-              <p align="center"><a href="http://cancercontrol.cancer.gov/">Division of Cancer Control & Population Sciences</a>, 
-              <span style="white-space:nowrap">a Division of <a href="www.cancer.gov">National Cancer Institute</a></span><br>
-              BG 9609 MSC 9760 | 9609 Medical Center Drive | Bethesda, MD 20892-9760 | <span style="white-space:nowrap"><a target="_blank" value="+18004006916" href="tel:1-800-422-6237">1-800-4-CANCER</a></span>
-              </p></div>"""
-            if self.EMAIL_BODY is None:
-                message = """<head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'><title>html title</title></head><body>{0}{1}{2}</body>""".format(header, body, footer)
-            else:
-                message = """<head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'><title>html title</title></head><body>{0}</body>""".format(self.EMAIL_BODY)
-
-            print "sending E-Mail"
-            self.createMail(sendTo=data['queue']['email'],message=message, files=files)
-            print "Queue job DONE!"
-            resp = jsonify({ 
-                "message": "The batch process has begun. The results will be emailed to '" + recipient + "'"
-                })
-        except Exception, e:
-            print "Error Type: {0} Error: {1} Trace: {2}".format(errorType, error, traceback)
-            resp = jsonify({"message": e.args})
-            resp.status_code = 400
-        finally:
-            return resp
+    PROCESS_METHOD = None
+    CONSUMER = None
 
     @defer.inlineCallbacks
     def startQueue(self):
+        print "in startQueue: "
         try:
-            print "in startQueue"
-
             if self.QUEUE is not None:
                 client = yield Stomp(self.QUEUE).connect()
                 headers = {
-                    # client-individual mode is necessary for
-                    # concurrent processing (requires ActiveMQ >= 5.2)
                     StompSpec.ACK_HEADER: StompSpec.ACK_CLIENT_INDIVIDUAL,
-                    # the maximal number of messages the broker will let 
-                    # you work on at the same time
                     'activemq.prefetchSize': self.PREFETCH
                 }
 
-                client.subscribe(self.Q_NAME, headers, listener=self)
+                client.subscribe(self.QUEUE, headers, listener=SubscriptionListener(self.CONSUMER, errorDestination=self.Q_ERROR))
+                client.add(listener=self)
         except Exception, e:
-            errorType, error, traceback = sys.exc_info()
-            print "Error Type: {0} Error: {1} \n {2}".format(errorType, error, traceback)
-
-    def processCleanup(self, connect):
-        print "in proccessCleanup, Cleaning up...."
-        print "<-- Add code for cleanup after queue process complete, if necessary -->"
+            errorType, error,traceback = sys.exc_info()
+            print "ErrorType: {0}\n Error: {1}\n traceback: {2} at line number '{3}' in {4}".format(errorType, error, traceback, traceback.tb_lineno, __file__)
 
     def queueConnectionLost(self, connect, reason):
-        print "in queueConnectionLost, Connection lost at {0}".format(time.strftime("%a, %d %b %Y %H:%M:%S"))
+        print "Connection lost at {0} \n Due to {1}".format(time.strftime("%a, %d %b %Y %H:%M:%S"), reason)
+        super(QueueProcessor, self).onConnectionLost(connect, reason)
         self.startQueue()
 
-    def __init__(self, CONFIG_FILE=None, Q_NAME=None, Q_URL=None, Q_PORT=None, Q_ERROR=None, PRODUCT_NAME="", MAIL_HOST=None, MAIL_ADMIN=None, EMAIL_BODY=None, PREFETCH=100):
+    def __init__(self, CONFIG_FILE=None, Q_NAME=None, Q_URL=None, Q_ERROR=None, PRODUCT_NAME=None, MAIL_HOST=None, MAIL_ADMIN=None, EMAIL_BODY=None, PREFETCH=100, PROCESS_FUNCT=None, CONSUMER_FUNCT=None):
+        print "QueueProcessor init"
         if CONFIG_FILE is not None:
             self.CONFIG_FILE = CONFIG_FILE
             self.QUEUE_CONFIG = PropertyUtil(self.CONFIG_FILE)
-            self.QUEUE = StompConfig(self.queueConfig[Q_URL])
-            self.Q_NAME = self.QUEUE_CONFIG[Q_NAME]
             self.Q_URL = self.QUEUE_CONFIG[Q_URL]
+            self.QUEUE = StompConfig(self.QUEUE_CONFIG[Q_URL])
+            self.Q_NAME = self.QUEUE_CONFIG[Q_NAME]
             self.Q_ERROR = self.QUEUE_CONFIG[Q_ERROR]
+
             self.MAIL_HOST = self.QUEUE_CONFIG[MAIL_HOST]
             self.MAIL_ADMIN = self.QUEUE_CONFIG[MAIL_ADMIN]
             self.PRODUCT_NAME = PRODUCT_NAME
-            self.PREFETCH = int(PREFETCH)
-            self.Q_PORT = int(self.QUEUE_CONFIG[Q_PORT])
+            self.PREFETCH = str(PREFETCH)
 
-            print self
-            super(QueueProcessor, self).__init__(self.queueConsumer)
+            self.PROCESS_METHOD = PROCESS_FUNCT
+            self.CONSUMER = CONSUMER_FUNCT
 
-            self.QUEUE = StompConfig(uri="failover:({0})?startupMaxReconnectAttempts=-1,initialReconnectDelay=300000".format(self.queueConfig[Q_URL]))
-            self.QUEUE_CONFIG = PropertyUtil(self.CONFIG_FILE)
-            self.rscript = robjects.r('"source(scriptName.R)"')
+            config = StompConfig(self.Q_URL)
+
+            self.startQueue()
 
 if __name__ == '__main__':
+    print "Queue Processor __main__"
     logging.basicConfig(level=logging.DEBUG)
     QueueProcessor().startQueue()
     reactor.run()
