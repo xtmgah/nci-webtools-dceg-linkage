362cf3145ce44b2dc44e5880f54d3a244f46e3ab
kjiang95 <kjiang95@umd.edu>
2018-01-11 17:28:21 -0500

    testing LDassoc svg export (sans glyphs) comment out hover

--- LDlink/LDassoc.py
+++ LDlink/LDassoc.py
@@ -927,26 +927,29 @@ def calculate_assoc(file,region,pop,request,myargs):
 	a = [coord1/1000000.0-whitespace,coord2/1000000.0+whitespace]
 	b = [-log10(0.00000005),-log10(0.00000005)]
 	assoc_plot.line(a, b, color="blue", alpha=0.5)
-
+	
+	###### commented out for testing #####
 	# assoc_points_not1000G=assoc_plot.circle(p_plot_pos, p_plot_pval, size=9+float("0.25")*14.0, source=source_p, line_color="gray", fill_color="white")
 	# assoc_plot.add_tools(HoverTool(renderers=[assoc_points_not1000G], tooltips=OrderedDict([("Variant", "@p_plot_pos2"), ("P-value", "@p_plot_pval2"), ("Distance (Mb)", "@p_plot_dist")])))
 
 	# assoc_points=assoc_plot.circle(x, y, size=size, source=source, color=color, alpha=alpha)
 	# hover=HoverTool(renderers=[assoc_points])
 
-	hover.tooltips=OrderedDict([
-		("Variant", "@prs @p_alle"),
-		("P-value", "@p_val"),
-		("Distance (Mb)", "@dist"),
-		("MAF", "@p_maf"),
-		("R"+sup_2+" ("+q_rs[0]+")", "@r"),
-		("D\' ("+q_rs[0]+")", "@d"),
-		("Correlated Alleles", "@alleles"),
-		("RegulomeDB", "@regdb"),
-		("Functional Class", "@funct"),
-	])
+	# hover.tooltips=OrderedDict([
+	# 	("Variant", "@prs @p_alle"),
+	# 	("P-value", "@p_val"),
+	# 	("Distance (Mb)", "@dist"),
+	# 	("MAF", "@p_maf"),
+	# 	("R"+sup_2+" ("+q_rs[0]+")", "@r"),
+	# 	("D\' ("+q_rs[0]+")", "@d"),
+	# 	("Correlated Alleles", "@alleles"),
+	# 	("RegulomeDB", "@regdb"),
+	# 	("Functional Class", "@funct"),
+	# ])
 
-	assoc_plot.add_tools(hover)
+	# assoc_plot.add_tools(hover)
+	###### commented out for testing #####
+	
 
 	# Annotate RebulomeDB scores
 	if myargs.annotate==True:
