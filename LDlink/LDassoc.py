305a62725b6176595ab89559c9dabb12e502d7c2
Kevin Jiang <21976531+kjiang95@users.noreply.github.com>
2018-01-16 11:30:06 -0500

    bokeh text fix rect glyphs

--- LDlink/LDassoc.py
+++ LDlink/LDassoc.py
@@ -1064,18 +1064,21 @@ def calculate_assoc(file,region,pop,request,myargs):
 			# DEBUG
 			gene_plot.segment(genes_plot_start, genes_plot_yn, genes_plot_end,
 							  genes_plot_yn, color="black", alpha=1, line_width=2)
-			# gene_plot.rect(x='exons_plot_x', y='exons_plot_yn', width='exons_plot_w', height='exons_plot_h',
-			# 			   source=source_gene_plot, fill_color="grey", line_color="grey")
-			gene_plot.rect(exons_plot_x, exons_plot_yn, exons_plot_w, exons_plot_h,
-						   fill_color="grey", line_color="grey")
+			gene_rects = gene_plot.rect(x='exons_plot_x', y='exons_plot_yn', width='exons_plot_w', height='exons_plot_h',
+						   source=source_gene_plot, fill_color="grey", line_color="grey")
+			# gene_plot.rect(exons_plot_x, exons_plot_yn, exons_plot_w, exons_plot_h,
+			# 			   fill_color="grey", line_color="grey")
 			gene_plot.text(genes_plot_start, genes_plot_yn, text=genes_plot_name, alpha=1, text_font_size="7pt",
 						   text_font_style="bold", text_baseline="middle", text_align="right", angle=0)
-			hover = gene_plot.select(dict(type=HoverTool))
-			hover.tooltips = OrderedDict([
-				("Gene", "@exons_plot_name"),
-				("Transcript ID", "@exons_plot_id"),
-				("Exon", "@exons_plot_exon"),
-			])
+
+			gene_plot.add_tools(HoverTool(renderers=[gene_rects], tooltips=OrderedDict([("Gene", "@exons_plot_name"),("Transcript ID", "@exons_plot_id"),("Exon", "@exons_plot_exon")])))
+			
+			# hover = gene_plot.select(dict(type=HoverTool))
+			# hover.tooltips = OrderedDict([
+			# 	("Gene", "@exons_plot_name"),
+			# 	("Transcript ID", "@exons_plot_id"),
+			# 	("Exon", "@exons_plot_exon"),
+			# ])
 
 		else:
 			x_coord_text = coord1/1000000.0 + (coord2/1000000.0 - coord1/1000000.0) / 2.0
@@ -1193,17 +1196,22 @@ def calculate_assoc(file,region,pop,request,myargs):
 			gene_c_plot.segment(genes_c_plot_start, genes_c_plot_yn, genes_c_plot_end,
 							  genes_c_plot_yn, color="black", alpha=1, line_width=2)
 			# DEBUG
-			# gene_c_plot.rect(x='exons_c_plot_x', y='exons_c_plot_yn', width='exons_c_plot_w', height='exons_c_plot_h',
-			# 			   source=source_gene_c_plot, fill_color="grey", line_color="grey")
-			gene_c_plot.rect(exons_c_plot_x, exons_c_plot_yn, exons_c_plot_w, exons_c_plot_h,
-						   fill_color="grey", line_color="grey")
+			gene_c_rects = gene_c_plot.rect(x='exons_c_plot_x', y='exons_c_plot_yn', width='exons_c_plot_w', height='exons_c_plot_h',
+						   source=source_gene_c_plot, fill_color="grey", line_color="grey")
+			# gene_c_plot.rect(exons_c_plot_x, exons_c_plot_yn, exons_c_plot_w, exons_c_plot_h,
+			# 			   fill_color="grey", line_color="grey")
 			gene_c_plot.text(genes_c_plot_start, genes_c_plot_yn, text=genes_c_plot_name, alpha=1, text_font_size="7pt",
 						   text_font_style="bold", text_baseline="middle", text_align="right", angle=0)
-			hover = gene_c_plot.select(dict(type=HoverTool))
+
+			# hover = gene_c_plot.select(dict(type=HoverTool))
+			hover = HoverTool(renderers=[gene_c_rects])
+
 			hover.tooltips = OrderedDict([
 				("Gene", "@exons_c_plot_name"),
 				("Transcript IDs", "@exons_c_plot_id"),
 			])
+
+			gene_c_plot.add_tools(hover)
 
 		else:
 			x_coord_text = coord1/1000000.0 + (coord2/1000000.0 - coord1/1000000.0) / 2.0
