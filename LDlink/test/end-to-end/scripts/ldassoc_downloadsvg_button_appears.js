1f9ddc7e68008ffbfebbc55bc018edb97cf5b266
Kevin Jiang <21976531+kjiang95@users.noreply.github.com>
2018-01-26 16:40:05 -0500

    add JS test files

---
+++ LDlink/test/end-to-end/scripts/ldassoc_downloadsvg_button_appears.js
@@ -0,0 +1,44 @@
+
+const assert = require('assert');
+const path = require('path');
+const test = require('selenium-webdriver/testing');
+const webdriver = require('selenium-webdriver'),
+By = webdriver.By,
+until = webdriver.until;
+
+describe(path.basename(__filename), function() {
+  // --enter test case name (ie. 'example test case')
+  test.it('ldassoc_downloadsvg_button_appears', function(done) {
+    this.timeout(0);
+    var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+    // -----example get path of example test file----- 
+    // --enter name of example input files folder in main directory (ie. 'examples')  
+    // let examplesDirectory = __dirname.split(path.sep).concat(['examples']);
+
+    // --enter name of file (ie. 'study2.txt')
+    // driver.findElement(By.id("study_1")).sendKeys(examplesDirectory.concat(['study2.txt']).join(path.sep)).then(function() {
+    //   driver.sleep(1000);
+    // });
+
+    driver.get("https://analysistools-sandbox.nci.nih.gov"+"/");
+		driver.sleep('2000');
+		driver.findElement(By.linkText("LDLink")).click();
+		driver.sleep('2000');
+		driver.findElement(By.id("ldassoc-tab-anchor")).click();
+		driver.sleep('2000');
+		driver.findElement(By.css("span.slider.round")).click();
+		driver.sleep('5000');
+		driver.findElement(By.id("ldassoc")).click();
+		driver.sleep('8000');
+		driver.findElement(By.id("ldassoc-downloadSVG")).getText().then(text=> {
+			assert(text == '');
+			done();
+		});
+		
+
+    driver.close();
+  });
+})
