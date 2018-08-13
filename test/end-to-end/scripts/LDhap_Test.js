f022510cefc04bc39aed7b387bb46bbec185d036
Kevin Jiang <kvvnjng@gmail.com>
2018-08-13 11:53:47 -0400

    Update LDhap_Test.js

--- test/end-to-end/scripts/LDhap_Test.js
+++ test/end-to-end/scripts/LDhap_Test.js
@@ -56,7 +56,7 @@ describe('Test Suite 1 - ' + path.basename(__filename), function() {
 		driver.sleep('2000');
 		driver.findElement(By.id(`ldhap-tab-anchor`)).click();
 		driver.sleep('2000');
-		driver.findElement(By.id(`ldhap-file`)).sendKeys('/home/ncianalysis/.jenkins/jobs/LDlink-sandbox-testing-only/workspace/test/end-to-end/test-data/5_sample_LDhap.txt');
+		driver.findElement(By.id(`ldhap-file`)).sendKeys('/home/ncianalysis/.jenkins/jobs/LDlink-dev-testing-only/workspace/test/end-to-end/test-data/5_sample_LDhap.txt');
 		driver.sleep('2000');
 		driver.findElement(By.xpath(`(//button[@type='button'])[6]`)).click();
 		driver.sleep('2000');
